93bbf8fd08ca5579253353130de619adf7330834
beomsu1 <beumsu1221@gmail.com>
2024-01-25 18:42:37 +0900

    화면 페이지처리

--- src/main/resources/static/js/bookPaging.js
+++ src/main/resources/static/js/bookPaging.js
@@ -1,28 +1,28 @@
 function makePage(page, size, total) {
     let pagingResult = "";
-
-    const startNum = (Math.ceil(page / 9) * 9) - 8;
-
-    startNum === 1 ? "" : pagingResult += `<a href="${startNum - 1}" class="btn btn-primary"><</a>`;
-
-    // 페이징 번호 변수
-    let temp = startNum;
-
-    // 페이징 버튼 동적 생성 / while(true) 무한 반복문 사용
-    while (true) {
-        // 페이징 번호 * 9 이 total 보다 크면 break
-        if (temp * size > total) {
-            break;
-        }
-
-        // temp == page -> 활성화
-        temp === page ? pagingResult += `<a href="${temp}" class="btn btn-primary active">${temp}</a>` : pagingResult += `<a href="${temp}" class="btn btn-primary">${temp}</a>`;
-
-        temp++;
+  
+    // 전체 페이지 수 계산
+    const totalPages = Math.ceil(total / size);
+  
+    // 이전 페이지 버튼 생성
+    if (page > 1) {
+      pagingResult += `<a href="${page - 1}" class="btn btn-primary"><</a>`;
     }
-
-    // total이 size * 9을 나눈 나머지가 0보다 크면 다음 페이지 노출
-    total % (size * 9) > 0 ? pagingResult += `<a href="${temp}" class="btn btn-primary">></a>` : "";
-
+  
+    // 페이징 번호 생성
+    for (let i = 1; i <= totalPages; i++) {
+      if (i === page) {
+        pagingResult += `<a href="${i}" class="btn btn-primary active">${i}</a>`;
+      } else {
+        pagingResult += `<a href="${i}" class="btn btn-primary">${i}</a>`;
+      }
+    }
+  
+    // 다음 페이지 버튼 생성
+    if (page < totalPages) {
+      pagingResult += `<a href="${page + 1}" class="btn btn-primary">></a>`;
+    }
+  
     return pagingResult;
-}
+  }
+  
