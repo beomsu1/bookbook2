ee5495ed9897e52beb58894af2ba2b3cfe827604
beomsu1 <beumsu1221@gmail.com>
2024-02-16 19:09:21 +0900

    save

--- src/main/java/org/bs/rental/controller/reply/BoardReplyController.java
+++ src/main/java/org/bs/rental/controller/reply/BoardReplyController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,7 +40,7 @@ public class BoardReplyController {
 
     // Board Reply Create
     @PostMapping("{tno}/create")
-    public ResponseEntity<String> postBoardReplyCreate(@PathVariable("tno") Long tno, BoardReplyCreateDTO boardReplyCreateDTO){
+    public ResponseEntity<String> postBoardReplyCreate(@PathVariable("tno") Long tno, @RequestBody BoardReplyCreateDTO boardReplyCreateDTO){
 
         log.info("POST | Board Reply Create Controller");
 
