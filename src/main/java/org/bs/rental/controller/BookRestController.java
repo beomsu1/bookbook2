4acf014b2b9de7d61f8c7ffd5319b08d243b85b7
beomsu1 <beumsu1221@gmail.com>
2024-02-20 00:15:09 +0900

    회원이 빌린 책 리스트 추가

--- src/main/java/org/bs/rental/controller/BookRestController.java
+++ src/main/java/org/bs/rental/controller/BookRestController.java
@@ -3,7 +3,6 @@ package org.bs.rental.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.bs.rental.dto.book.BookListByMemberDTO;
-import org.bs.rental.dto.book.BookListDTO;
 import org.bs.rental.service.Book.BookService;
 import org.bs.rental.util.page.PageRequestDTO;
 import org.bs.rental.util.page.PageResponseDTO;
